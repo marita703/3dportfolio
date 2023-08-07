1e247f11f5b0250efd77c95308640aaea55e06cd
Maria del Mar <chocoinformatics2016@gmail.com>
2023-08-07 12:06:13 +0200

    deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputerCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-scren mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center intems-center mt-5"></div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
